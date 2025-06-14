3cccf5211a179e2a653b34a8f1e5c0b357da8def
Marcosvbs11 <mvbs9@discente.ifpe.edu.br>
2025-06-13 22:19:18 -0300

    atividade_04

---
+++ atividade_02/database/migrations/2025_06_06_140328_create_books_table.php
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('author_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('publisher_id')->constrained()->onDelete('cascade');
+            $table->integer('published_year')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
---
+++ atividade_02/database/migrations/2025_06_06_140744_add_published_year_to_books_table.php
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            if (!Schema::hasColumn('books', 'published_year')) {
+                $table->integer('published_year');
+            }
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('published_year');
+        });
+    }
+};
